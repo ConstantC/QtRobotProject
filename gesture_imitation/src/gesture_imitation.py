e026a2e2952ebaadd46f454205f4e10b688f4bb8
QTrobot <>
2023-03-29 11:47:12 +0200

    gesture update

--- gesture_imitation/src/gesture_imitation.py
+++ gesture_imitation/src/gesture_imitation.py
@@ -12,6 +12,9 @@ from cv_bridge import CvBridge, CvBridgeError
 from qt_nuitrack_app.msg import Faces, FaceInfo
 
 
+speechSay = rospy.ServiceProxy('/qt_robot/speech/say', speech_say)
+emotionShow = rospy.ServiceProxy('/qt_robot/emotion/show', emotion_show)
+
 class image_converter:
     faces = None
     faces_time = None   
@@ -34,71 +37,43 @@ class image_converter:
             print(e)
 
 
-if __name__ == '__main__':
+def rules():
+    try:
+        # call a ros service with text message
+        rospy.loginfo("Explaining rules !")
+        # speechSay("Je vais te montrer des mouvements et tu dois les imiter.")
+    except KeyboardInterrupt:
+        pass
+
+def end():
+    try:
+        # call a ros service with text message
+        rospy.loginfo("End of the exercise")
+        #speechSay("Fin de l'exercice, merci pour tes réponses !")
+    except KeyboardInterrupt:
+        pass
+
+def gesturesImitation(gesture):
     rospy.loginfo("Gesture imitation launched, waiting for the services to initialized!")
     
     # define the ros service
-    speechSay = rospy.ServiceProxy('/qt_robot/speech/say', speech_say)
-    recognize = rospy.ServiceProxy('/qt_robot/speech/recognize', speech_recognize)
     rospy.init_node('gesture_imitation_node')
     rospy.loginfo("gesture_imitation_node started!")
 
     # block/wait for ros service
     rospy.wait_for_service('/qt_robot/speech/say')
-    rospy.wait_for_service('/qt_robot/speech/recognize')
+    rospy.wait_for_service('/qt_robot/emotion/show')
     rospy.loginfo("Services initiated")
     ic = image_converter()
-    
-    # Fetch the questions 
-    rospy.loginfo("Selecting gesture:")
-    allGestures = ["test1","test2","test3","test4","test5","test6"]
-    gestures = []
-    
-    for i in range(3):
-        rng = random.randrange(len(allGestures))
-        gestures.append(allGestures[rng])
-        allGestures.pop(rng)
-    
-    rospy.loginfo("Gestures selected !")
 
+    
     try:
-        # call a ros service with text message
-        rospy.loginfo("Explaining rules !")
-        # speechSay("Je vais te montrer des mouvements et tu dois les imiter.")
-        score = 0
-        interaction = 0
-        for i in range(len(gestures)):
-            rospy.loginfo("Gesture: %s", gestures[i])
-            
-            if any(answer in resp.transcript for answer in answers[i]):
-                #speechSay("Super c'est la bonne réponse")
-                rospy.loginfo("Acceptable answer")
-                score += 1
-                interaction += 1
-
-            elif any(question in resp.transcript for question in question.split()):
-                print(question.split())
-                print(any(question in resp.transcript for question in question.split()))
-                #speechSay("Il ne faut pas répéter la question mais y répondre.")
-                rospy.loginfo("The question has been repeated by the children")
-                i = i-1
-                interaction += 1
-
-            elif not resp.transcript:
-                #speechSay("Je n'ai pas bien entendu ta réponse.")
-                rospy.loginfo("No answer detected")
-                i = i-1
-                
-            else:
-                #speechSay("Merci pour ta réponse.")
-                rospy.loginfo("Answer is not right")
-                interaction += 1
-
-        rospy.loginfo("End of the exercise")
-        #speechSay("Fin de l'exercice, merci pour tes réponses !")
-        finalScore = str(int(score/interaction*100))
-        rospy.loginfo("The answers are %s pourcent right !", finalScore)
+        rospy.loginfo("Gesture: %s", gesture)
+        emotionShow('gestures/' + gesture)
+                        
+        rospy.loginfo("End of the gesture")
     except KeyboardInterrupt:
         pass
 
-    rospy.loginfo("finsihed!")
+gesturesImitation('wave')
+rospy.loginfo("finsihed!")
